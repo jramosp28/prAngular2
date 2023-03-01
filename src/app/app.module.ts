a575af56df167f704f9a299d4e39b14ce2875b41
jramosp28 <javierrampob@campus.monlau.com>
2023-03-01 20:15:11 +0100

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { FormulariosComponent } from './body/formularios/formularios.component';
+import { ClientesComponent } from './body/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    FormulariosComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
